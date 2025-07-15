f6e983bfd3c39db9cad21d8f24ab2c3b886b81f1
Ramz001 <rkenjaev1@gmail.com>
2025-07-15 07:54:19 +0500

    Update Jest configuration to set test environment to Node and ignore specific paths during testing

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/"
+  ],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
